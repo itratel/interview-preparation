81cd378561f825e5802909faa8e5e9a89713135c
whd <whd.java@gmail.com>
2019-10-03 23:34:58 +0800

    modify springContext

--- src/main/java/com/whd/interview/preparation/utils/PropertiesContext.java
+++ src/main/java/com/whd/interview/preparation/utils/PropertiesContext.java
@@ -1,54 +1,32 @@
 package com.whd.interview.preparation.utils;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import static com.whd.interview.preparation.utils.SpringContext.getApplicationContext;
+
 /**
  * @author whd.java@gmail.com
- * @date 2019/10/03 18:34
+ * @date 2019/10/03 23:22
  * @apiNote Describe the function of this class in one sentence
  */
-@Component
-public class PropertiesContext implements ApplicationContextAware {
+public class PropertiesContext {
 
-    private static ApplicationContext applicationContext = null;
-
-    /**ResourceLoaderAware
-     * Set the ApplicationContext that this object runs in.
-     * Normally this call will be used to initialize the object.
-     * <p>Invoked after population of normal bean properties but before an init callback such
-     * as {@link InitializingBean#afterPropertiesSet()}
-     * or a custom init-method. Invoked after {@link ResourceLoaderAware#setResourceLoader},
-     * {@link ApplicationEventPublisherAware#setApplicationEventPublisher} and
-     * {@link MessageSourceAware}, if applicable.
-     *
-     * @param applicationContext the ApplicationContext object to be used by this object
-     * @throws BeansException if thrown by application context methods
-     * @see BeanInitializationException
-     */
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        PropertiesContext.applicationContext = applicationContext;
-    }
+    private static final ApplicationContext APPLICATION_CONTEXT = getApplicationContext();
 
     /***
      * 获取上下文环境
      * @return {@link Environment}
      */
     private static Environment getEnvironment() {
-        if (Objects.nonNull(applicationContext)) {
-            return applicationContext.getEnvironment();
+        if (Objects.nonNull(APPLICATION_CONTEXT)) {
+            return APPLICATION_CONTEXT.getEnvironment();
         }
         throw new RuntimeException("上下文为空");
     }
@@ -70,14 +48,6 @@ public class PropertiesContext implements ApplicationContextAware {
      */
     public static <T> T getProEntity(String proPrefix, Class<T> clazz) {
         return getBinder().bind(proPrefix, Bindable.of(clazz)).orElseCreate(clazz);
-//        return getBinder().bind(proPrefix, Bindable.of(clazz)).orElseGet(() -> {
-//            try {
-//                return clazz.newInstance();
-//            } catch (InstantiationException | IllegalAccessException e) {
-//                e.printStackTrace();
-//                return null;
-//            }
-//        });
     }
 
     /***
